04b71f83838e411801896abe0480f03de972cd01
Nickolay Grigoriev <gnarus@inbox.ru>
2023-04-09 18:00:38 +0300

    4 commit

--- Task2.py
+++ Task2.py
@@ -9,5 +9,5 @@
 '''
 str1 = input("Введите целое положительное число: ")
 list1 = list(str1)
-sum1 = sum(map(int, list1))
-print(f"{str1} -> {sum1} ({list1[0]} + {list1[1]} + {list1[2]})")
+sum1 = sum(map(int, list1)) # нахожу сумму цифр
+print(f"{str1} -> {sum1} ({list1[0]} + {list1[1]} + {list1[2]})") # Вывод результата на экран
