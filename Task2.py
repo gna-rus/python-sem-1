f89204abee48ee47dfc973751a50c26d16b3a75f
Nickolay Grigoriev <gnarus@inbox.ru>
2023-04-09 17:40:06 +0300

    2 Commit

--- Task2.py
+++ Task2.py
@@ -7,5 +7,7 @@
 100 -> 1 (1 + 0 + 0) |
 
 '''
-
-print(sum(map(int, list(input("Введите целое положительное число: ")))))
+str1 = input("Введите целое положительное число: ")
+list1 = list(str1)
+sum1 = sum(map(int, list1))
+print(f"{str1} -> {sum1} ({list1[0]} + {list1[1]} + {list1[2]})")
